1c068c65f329abd02972f11eea019361ac90ed5f
vrushabhdhatrak <vrushabhdhatral10@gmail.com>
2021-03-21 17:00:59 +0530

    all chanes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDahe72yghG4q3cpHYYDl85zdi17kllzX0",
+    authDomain: "courseproject-fde9e.firebaseapp.com",
+    databaseURL: "https://courseproject-fde9e.firebaseio.com",
+    projectId: "courseproject-fde9e",
+    storageBucket: "courseproject-fde9e.appspot.com",
+    messagingSenderId: "30320175867",
+    appId: "1:30320175867:web:799005f25d272bcae89e5e",
+    measurementId: "G-7PYFL2M8J8"
+  }
 };
